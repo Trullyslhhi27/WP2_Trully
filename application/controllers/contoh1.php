f5b21384cd5cf903763424192ca7bc2e6e047de7
Trullyslhhi27 <trullysilalahi@gmail.com>
2023-09-27 12:22:49 +0700

    menambahkan /application,/system,.editorconfig,.gitignore,composer.json,index.php,licensi.txt,readme.rst

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama Saya Trully Silalahi
+        Saya Tinggal Di Daerah Bekasi
+        olahraga yang saya sukai adalah 
+        sepakbola";
+    }
+}
