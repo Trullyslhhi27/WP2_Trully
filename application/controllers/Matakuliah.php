6b713ed5627881ba8d1e64a0c55febe0461d00b1
trullyslhhi27 <trullysilalahi@gmail.com>
2023-10-16 19:48:35 +0700

    Pert 4 Memuat Library form validation

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -11,12 +11,28 @@ class Matakuliah extends CI_Controller
 
     public function cetak()
     {
-        $data = [
-            'kode' => $this->input->post('kode'),
-            'nama' => $this->input->post('Nama'),
-            'sks' => $this->input->post('sks'),
-        ];
+        $this->form_validation->set_rules('kode', 'Kode Matakuliah', 
+'required|min_lenght[3]', [
+            'requried' => 'Kode Matakuliah Harus diisi',
+            'min_lenght' => 'Kode terlalu pendek'
+        ]);
 
-        $this->load->view('view-data-matakuliah', $data);
+        $this->form_validation->set_rules('nama', 'Nama Matakuliah', 
+'required|min_lenght[3]', [
+            'requried' => 'Nama Matakuliah Harus diisi',
+            'min_lenght' => 'Nama terlalu pendek'
+        ]);
+
+        if ($this->form_validation->run() != true) {
+            $this->load->view('view-form-matakuliah');
+        } else {
+            $data = [
+                'kode' => $this->input->post('kode'),
+                'nama' => $this->input->post('nama'),
+                'sks' => $this->input->post('sks'),
+            ];
+
+            $this->load->view('view-data-matakuliah', $data);
+        }
     }
 }
