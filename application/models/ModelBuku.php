5fc8009a4ea18e60b3f5aa017b666952bfa243f9
Trullyslhhi27 <trullysilalahi@gmail.com>
2023-12-08 00:18:14 +0700

    P11 P12 Memodelkan Data Buku

--- application/models/ModelBuku.php
+++ application/models/ModelBuku.php
@@ -12,18 +12,22 @@ class ModelBuku extends CI_Model
     {
         return $this->db->get_where('buku', $where);
     }
+//ModelBuku.php//
     public function simpanBuku($data = null)
     {
         $this->db->insert('buku',$data);
     }
+
     public function updateBuku($data = null, $where = null)
     {
         $this->db->update('buku', $data, $where);
     }
+
     public function hapusBuku($where = null)
     {
         $this->db->delete('buku', $where);
     }
+
     public function total($field, $where)
     {
         $this->db->select_sum($field);
@@ -33,35 +37,39 @@ class ModelBuku extends CI_Model
         $this->db->from('buku');
         return $this->db->get()->row($field);
     }
-
+    
     //manajemen kategori
     public function getKategori()
     {
         return $this->db->get('kategori');
     }
+
     public function kategoriWhere($where)
     {
         return $this->db->get_where('kategori', $where);
     }
+
     public function simpanKategori($data = null)
     {
         $this->db->insert('kategori', $data);
     }
+
     public function hapusKategori($where = null)
     {
         $this->db->delete('kategori', $where);
     }
+
     public function updateKategori($where = null, $data = null)
     {
         $this->db->update('kategori', $data, $where);
     }
+
     //join
     public function joinKategoriBuku($where)
     {
         $this->db->select('buku.id_kategori,kategori.kategori');
         $this->db->from('buku');
-        $this->db->join('kategori','kategori.id =
-buku.id_kategori');
+        $this->db->join('kategori','kategori.id = buku.id_kategori');
         $this->db->where($where);
         return $this->db->get();
     }
